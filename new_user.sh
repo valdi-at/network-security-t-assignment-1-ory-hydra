0ff4e5f69f3bca1d611707f5615863ad9a4b1fbb
Mohamad Valdi Ananda Tauhid <valdianandatauhid@proton.me>
2026-04-02 23:12:07 +0700

    feat: Shell scripts now ask caller

--- new_user.sh
+++ new_user.sh
@@ -1,18 +1,50 @@
 #!/bin/sh
+set -e
 
-curl -vX POST http://localhost:4434/admin/identities \
+while :; do
+    printf "Enter Oauth2 Provider (Hydra Admin) Server URI: "
+    IFS= read -r HYDRA_SERVER_URI
+
+    if [ -n "$HYDRA_SERVER_URI" ]; then
+        break
+    fi
+done
+
+while :; do
+    printf "Enter Username: "
+    IFS= read -r USERNAME
+
+    if [ -n "$USERNAME" ]; then
+        break
+    fi
+
+    printf "Username cannot be empty. Try again.\n"
+done
+
+while :; do
+    printf "Enter Password: "
+    IFS= read -r PASSWORD
+
+    if [ -n "$PASSWORD" ]; then
+        break
+    fi
+
+    printf "Password cannot be empty. Try again.\n"
+done
+
+echo curl -vX POST "$HYDRA_SERVER_URI/admin/identities" \
   -H "Content-Type: application/json" \
-  -d '{
-    "schema_id": "default",
-    "traits": {
-      "email": "shiny@shinyshoe.net",
-      "username": "shiny"
+  -d "{
+    \"schema_id\": \"default\",
+    \"traits\": {
+      \"email\": \"$USERNAME\",
+      \"username\": \"shiny\"
     },
-    "credentials": {
-      "password": {
-        "config": {
-          "password": "secret"
+    \"credentials\": {
+      \"password\": {
+        \"config\": {
+          \"password\": \"$PASSWORD\"
         }
       }
     }
-  }'
+  }"
